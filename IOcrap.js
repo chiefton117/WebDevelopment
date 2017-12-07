eb931eec037cb5f6d09eb02cbbb1b73db70c9834
MattBuffo <mattthel33t@gmail.com>
2017-12-07 14:15:17 -0500

    made io changes
    yeah

---
+++ IOcrap.js
@@ -0,0 +1,38 @@
+let questionid = 0;
+      let previousAnswers = [null];
+
+    	function reloadInterface(buttonnum) { // refreshes the interface and logs past responces
+        switch(buttonnum){
+          case 1: //use the responce id to get the goto of the question, add the responce and question to list of previous responces and questions, and reload the interface based on the new question
+          break;
+          case 2:
+          break;
+          case 3:
+          break;
+          case 4:
+          break;
+        }
+    		document.getElementById("prompt").innerHTML = 'prompt';
+        document.getElementById("Button1").innerHTML = 'bootan';
+        document.getElementById("Button2").innerHTML = 'bootan2';
+        document.getElementById("Button3").innerHTML = 'bootan3';
+        document.getElementById("Button4").innerHTML = 'bootan4';
+        previousAnswers = previousAnswers.push(questionid);
+        questionid = getNextQuestion();
+        console.log(questionid);
+    	}
+      const getQuestionByID = id =>{
+        switch(id){
+          
+        }
+      } 
+      const getNextQuestion = rID =>{ // gets the question id based on the responce goto
+        switch(rID){
+          default: 
+            return 0;
+            break;
+        }
+      }
+      const getResponces = id =>{ //gets array of responces based on question id
+        
+      }
