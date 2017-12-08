f499d892fa6b016e31c6f2af3900c6008e8bf645
MattBuffo <mattthel33t@gmail.com>
2017-12-08 10:54:03 -0500

    made it look good

--- js/backendCrap.js
+++ js/backendCrap.js
@@ -1,4 +1,6 @@
-function generateButtons(id){
+      var questionID;
+
+      function generateButtons(id){
         switch(id){
           case 0: 
           return false;
@@ -18,33 +20,32 @@ function generateButtons(id){
         }
       }
       const getQuestionByID = id =>{
-        switch(id){
-          
+        switch(id){ 
           case 0: 
-          return "Please add any information we should know below:";
-          break;
+            return "Please add any information we should know below:";
+            break;
           case 1: 
-          return "What is your favorite animal?";
-          break;
+            return "What is your favorite animal?";
+            break;
           case 2: 
-          return "What is your favorite breed of dog?";
-          break;
+            return "What is your favorite breed of dog?";
+            break;
           case 3: 
-          return "Does your favorite type of bird fly?";
-          break; 
+            return "Does your favorite type of bird fly?";
+            break; 
           default: 
-          return "Please add any information we should know below:";
-          break;
+            return "Please add any information we should know below:";
+            break;
         }
       } 
       const getGoTo = rID =>{ // gets the question id based on the responce goto
         switch(rID){
           case r1:
-          return 1;
-          break;
+            return 1;
+            break;
           case r3:
-          return 3;
-          break;
+            return 3;
+            break;
           default: 
             return 0; // should call gmail function
             break;
@@ -53,16 +54,16 @@ function generateButtons(id){
       const getResponces = id =>{ //gets array of responces based on question id
         switch(id){
           case 1: 
-          return ['r1','r2','r3','r4'];
-          break;
+            return ['r1','r2','r3','r4'];
+            break;
           case 2:
-          return ['r5','r6','r7','r8'];
-          break;
+            return ['r5','r6','r7','r8'];
+            break;
           case 3:
-          return ['r9','r10'];
-          break;
+            return ['r9','r10'];
+            break;
           default: 
-          return '';
-          break;
+            return '';
+            break;
         }
       }
